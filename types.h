c1680559e54399821c24c20ded28ff683f3e0839
ravikumar-12-creator <mavuriravi2005@gmail.com>
2026-09-25 21:56:52 +0530

    Create types.h

---
+++ types.h
@@ -0,0 +1,20 @@
+/*****************************************************************************
+ * @file      types.h
+ * @brief     Independent Data Type Definitions for Embedded C Projects
+ * @details   Standardized custom type abbreviations including const and volatile qualifiers.
+ *****************************************************************************/
+
+#ifndef _TYPES_H
+#define _TYPES_H
+
+typedef const unsigned char  cu8;
+typedef volatile unsigned int vu32;
+typedef unsigned char        u8;
+typedef signed char          s8;
+typedef unsigned short int   u16;
+typedef signed short int     s16;
+typedef unsigned int         u32;
+typedef signed int           s32;
+typedef float                f32;
+
+#endif /* _TYPES_H */
